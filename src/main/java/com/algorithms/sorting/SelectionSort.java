66ba8a7abc9f2a029f23d660138d30fce06b4cf1
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-06 18:15:41 +0530

    Add SelectionSort

---
+++ src/main/java/com/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.algorithms.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+
+	public static void main(String[] args) {
+		int arr[] = {5,7,4,3,2,1};
+		SelectionSort bs = new SelectionSort();
+		bs.sort(arr);
+		System.out.println(Arrays.toString(arr) ); 
+	}
+
+	public void sort(int[] arr) {
+		int n = arr.length;
+		for(int i = 0; i < n-1; i++ ) {
+			int min = i;
+			for(int j = i+1; j < n; j++) {
+				if(arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+
+		}
+	}
+}
